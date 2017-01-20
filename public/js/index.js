21b7ce1b7ec0a6643158b812d9f91326535078d2
Karthik <karthikramachandra93@gmail.com>
2017-01-20 12:22:00 +0530

    style mods for button

--- public/js/index.js
+++ public/js/index.js
@@ -16,15 +16,26 @@ socket.on('newMessage', function (message) {
 });
 
 
+socket.on('newLocationMessage', function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  var messageTextBox=jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(){
-
-
+      messageTextBox.val('')
   });
 });
 
@@ -34,12 +45,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending locaiton...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+
     alert('Unable to fetch location.');
   });
 });
